0bd8895c7d6bd85c8571c47816941ebf9fd6d3dc
nicolozibra1 <nico.zibra@gmail.com>
2023-03-28 15:52:37 +0200

    script-done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            titolo: 'Hello Vue!',
+            color: 'text-white',
+            image: '../img/web-developer-image.jpg',
+        }
+    },
+}).mount('#app');  
